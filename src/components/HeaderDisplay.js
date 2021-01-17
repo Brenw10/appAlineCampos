ac93fd4916b3dbfaa26392d85662c80e7815f264
Brendon Mota <bmota@its.jnj.com>
2021-01-17 15:58:44 -0300

    Creating Animated Component

---
+++ src/components/HeaderDisplay.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useRef } from 'react';
+import { StyleSheet, View, Animated } from 'react-native';
+
+function HeaderDisplay(props) {
+  const duration = useRef(props.duration).current;
+  const flexTopAnim = useRef(new Animated.Value(1 - props.flex)).current;
+  const flexBottomAnim = useRef(new Animated.Value(props.flex)).current;
+  const imageSizeAnim = useRef(new Animated.Value(props.imageSize)).current;
+  const opacityAnim = useRef(new Animated.Value(1)).current;
+  const propsRef = useRef(props);
+
+  useEffect(() => {
+    if (props !== propsRef.current) anim();
+    propsRef.current = props;
+  }, [props.flex, props.imageSize]);
+
+  function anim() {
+    Animated.parallel([
+      Animated.timing(flexTopAnim, {
+        toValue: 1,
+        duration,
+        useNativeDriver: false,
+      }),
+      Animated.timing(flexBottomAnim, {
+        toValue: 0,
+        duration,
+        useNativeDriver: false,
+      }),
+      Animated.timing(opacityAnim, {
+        toValue: 0,
+        duration,
+        useNativeDriver: false,
+      }),
+    ]).start(() => {
+      if (props.firstAnimDone) props.firstAnimDone();
+      Animated.parallel([
+        Animated.timing(flexTopAnim, {
+          toValue: 1 - props.flex,
+          duration,
+          useNativeDriver: false,
+        }),
+        Animated.timing(flexBottomAnim, {
+          toValue: props.flex,
+          duration,
+          useNativeDriver: false,
+        }),
+        Animated.timing(imageSizeAnim, {
+          toValue: props.imageSize,
+          duration,
+          useNativeDriver: false,
+        }),
+        Animated.timing(opacityAnim, {
+          toValue: 1,
+          duration,
+          useNativeDriver: false,
+        }),
+      ]).start(() => {
+        if (props.secondAnimDone) props.secondAnimDone();
+      });
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      <Animated.View style={{ ...styles.topContainer, flex: flexTopAnim }}>
+        <Animated.Image style={{ width: imageSizeAnim, height: imageSizeAnim }}
+          source={require('../assets/ic_launcher_round.png')} />
+      </Animated.View>
+      <Animated.View style={{ ...styles.bottomContainer, flex: flexBottomAnim }}>
+        <Animated.View style={{ flex: 1, opacity: opacityAnim }}>
+          {props.children}
+        </Animated.View>
+      </Animated.View>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#01877c',
+    flex: 1,
+  },
+  topContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomContainer: {
+    backgroundColor: '#FFF',
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    padding: 30,
+    paddingTop: 40,
+  },
+});
+
+export default HeaderDisplay;
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,95 +1,30 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Image, Animated } from 'react-native';
-import UserLogin from '../components/UserLogin';
+import HeaderDisplay from '../components/HeaderDisplay';
 import WelcomeLogin from '../components/WelcomeLogin';
+import UserLogin from '../components/UserLogin';
 
 function Login() {
   const [userlogin, setUserLogin] = useState();
-
-  const duration = 500;
-  const flexTopAnim = new Animated.Value(0.65);
-  const flexBottomAnim = new Animated.Value(0.35);
-  const imageSizeAnim = new Animated.Value(200);
-  const opacityAnim = new Animated.Value(1);
+  const [flex, setFlex] = useState(0.35);
+  const [imageSize, setImageSize] = useState(200);
 
   function onClickStart() {
-    Animated.sequence([
-      Animated.parallel([
-        Animated.timing(flexTopAnim, {
-          toValue: 1,
-          duration,
-          useNativeDriver: false,
-        }),
-        Animated.timing(flexBottomAnim, {
-          toValue: 0,
-          duration,
-          useNativeDriver: false,
-        }),
-        Animated.timing(imageSizeAnim, {
-          toValue: 130,
-          duration,
-          useNativeDriver: false,
-        }),
-        Animated.timing(opacityAnim, {
-          toValue: 0,
-          duration,
-          useNativeDriver: false,
-        }),
-      ]),
-      Animated.parallel([
-        Animated.timing(flexTopAnim, {
-          toValue: 0.3,
-          duration,
-          delay: duration,
-          useNativeDriver: false,
-        }),
-        Animated.timing(flexBottomAnim, {
-          toValue: 0.7,
-          duration,
-          delay: duration,
-          useNativeDriver: false,
-        }),
-      ]),
-    ]).start(() => setUserLogin(true));
+    setFlex(0.6);
+    setImageSize(150);
   }
 
   return (
-    <View style={styles.container}>
-      <Animated.View style={{ ...styles.topContainer, flex: flexTopAnim }}>
-        <Animated.Image style={{ width: imageSizeAnim, height: imageSizeAnim }}
-          source={require('../assets/ic_launcher_round.png')} />
-      </Animated.View>
-      <Animated.View style={{ ...styles.bottomContainer, flex: flexBottomAnim }}>
+    <>
+      <HeaderDisplay flex={flex} imageSize={imageSize} duration={500}
+        firstAnimDone={() => setUserLogin(true)}>
         {
-          !userlogin &&
-          <Animated.View style={{ flex: 1, opacity: opacityAnim }}>
-            <WelcomeLogin onClickStart={() => onClickStart()} />
-          </Animated.View>
+          userlogin
+            ? <UserLogin />
+            : <WelcomeLogin onClickStart={() => onClickStart()} />
         }
-        {
-          userlogin && <UserLogin />
-        }
-      </Animated.View>
-    </View>
+      </HeaderDisplay>
+    </>
   )
 };
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#01877c',
-    flex: 1,
-  },
-  topContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  bottomContainer: {
-    backgroundColor: '#FFF',
-    borderTopLeftRadius: 40,
-    borderTopRightRadius: 40,
-    padding: 30,
-    paddingTop: 40,
-  },
-});
 
 export default Login;
