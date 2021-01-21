4b9a53eedbb313c2ad43a486079367fb10f08bb5
Brendon Mota <bmota@its.jnj.com>
2021-01-21 11:14:17 -0300

    Removing unused consts

--- src/constants/screens.js
+++ src/constants/screens.js
@@ -1,28 +1,18 @@
 const CONSTANT = {
   INIT: {
     NAME: 'INIT',
-    FLEX: 0.35,
-    IMAGE_SIZE: 200,
   },
   LOGIN: {
     NAME: 'LOGIN',
-    FLEX: 0.7,
-    IMAGE_SIZE: 130,
   },
   ACTIONS: {
     NAME: 'ACTIONS',
-    FLEX: 0.6,
-    IMAGE_SIZE: 180,
   },
   SCHEDULE: {
     NAME: 'SCHEDULE',
-    FLEX: 0.75,
-    IMAGE_SIZE: 120,
   },
   SCHEDULING: {
     NAME: 'SCHEDULING',
-    FLEX: 0.75,
-    IMAGE_SIZE: 120,
   },
 };
 
