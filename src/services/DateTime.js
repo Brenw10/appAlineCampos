a20cdc3d13bdab12f49691758c2e5c47c5817451
Brendon Mota <bmota@its.jnj.com>
2021-01-26 20:43:21 -0300

    Removing unused DateTime functions

--- src/services/DateTime.js
+++ src/services/DateTime.js
@@ -2,10 +2,6 @@ import Moment from 'moment';
 
 Moment.locale('pt-BR');
 
-function getDateTimeFormat(date) {
-  return Moment(date).format('DD/MM/YYYY HH:mm');
-}
-
 function getDefaultDateFormat(date) {
   return Moment(date).format('YYYY-MM-DD');
 }
@@ -22,27 +18,10 @@ function addDate(date, field, value) {
   return new Date(Moment(date).add(value, field));
 }
 
-function getLastDateFromNextMonths(months) {
-  return Moment().set('month', months).endOf('month');
-}
-
-function getMonthStartDate(date) {
-  return Moment(date).startOf('month');
-}
-
-function getDaysInNextMonths(date, months) {
-  const lastDate = getLastDateFromNextMonths(months);
-  const index = Moment(lastDate).diff(date, 'days') + 1;
-  return [...Array(index).keys()].map(value => addDate(date, 'day', value));
-}
-
 const service = {
-  getDateTimeFormat,
   getDateFormat,
   addDate,
-  getDaysInNextMonths,
   getDefaultDateFormat,
-  getMonthStartDate,
   getHourFormat,
 };
 
