622728715252b172b1630cd261255552e8b7ed76
Brendon Mota <bmota@its.jnj.com>
2021-01-30 15:10:14 -0300

    Created AppointmentItem

---
+++ src/components/AppointmentItem.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { APPOINTMENT } from '../constants/Appointment';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import DateTime from '../services/DateTime';
+
+function AppointmentItem({ item }) {
+  return (
+    <View style={styles.item}>
+      <View style={styles.subItem}>
+        <View style={{ flex: 1 }}>
+          <Text style={styles.hour}>
+            {DateTime.getHourFormat(item.datetime)}
+            {' - '}
+            {DateTime.getHourFormat(DateTime.addDate(item.datetime, 'minute', item.duration))}
+          </Text>
+          <Text style={styles.patient}>{item.client.name}</Text>
+          <Text style={styles.treatments}>{item.treatments.map(value => value.name).join('\n')}</Text>
+        </View>
+        <Image
+          style={styles.image}
+          resizeMode='contain'
+          source={{
+            uri: item.client.photo,
+          }}
+        />
+      </View>
+      <View style={styles.statusContainer}>
+        <Icon name={APPOINTMENT[item.status].ICON} size={18} color={APPOINTMENT[item.status].COLOR} />
+        <Text style={styles.statusText}>
+          {APPOINTMENT[item.status].TEXT}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+	item: {
+		backgroundColor: '#FFF',
+		padding: 20,
+		margin: 10,
+	},
+	subItem: {
+		flexDirection: 'row',
+	},
+	hour: {
+		color: '#45535e',
+	},
+	patient: {
+		color: '#4b5863',
+		fontSize: 15,
+		marginTop: 10,
+	},
+	treatments: {
+		marginTop: 10,
+		color: '#9caebb',
+	},
+	image: {
+		width: 70,
+		height: 70,
+		borderRadius: 100,
+	},
+	statusContainer: {
+		marginTop: 15,
+		alignSelf: 'center',
+		flexDirection: 'row',
+	},
+	statusText: {
+		marginLeft: 5,
+	},
+});
+
+export default AppointmentItem;
--- src/components/ClientCalendar.js
+++ src/components/ClientCalendar.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
 import { Agenda } from 'react-native-calendars';
 import { LocaleConfig } from 'react-native-calendars';
 import { CALENDAR } from '../constants/Calendar';
 import DateTime from '../services/DateTime';
 import Appointment from '../services/Appointment';
-import { APPOINTMENT } from '../constants/Appointment';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import AppointmentItem from './AppointmentItem';
 
 LocaleConfig.defaultLocale = 'pt-BR';
 
@@ -41,83 +39,16 @@ function ClientCalendar() {
 		return data.reduce((obj, value) => Object.assign(obj, value), {});
 	}
 
-	function renderItem(item) {
-		return (
-			<View style={styles.item}>
-				<View style={styles.subItem}>
-					<View style={{ flex: 1 }}>
-						<Text style={styles.hour}>
-							{DateTime.getHourFormat(item.datetime)}
-							{' - '}
-							{DateTime.getHourFormat(DateTime.addDate(item.datetime, 'minute', item.duration))}
-						</Text>
-						<Text style={styles.patient}>{item.client.name}</Text>
-						<Text style={styles.treatments}>{item.treatments.map(value => value.name).join('\n')}</Text>
-					</View>
-					<Image
-						style={styles.image}
-						resizeMode='contain'
-						source={{
-							uri: item.client.photo,
-						}}
-					/>
-				</View>
-				<View style={styles.statusContainer}>
-					<Icon name={APPOINTMENT[item.status].ICON} size={18} color={APPOINTMENT[item.status].COLOR} />
-					<Text style={styles.statusText}>
-						{APPOINTMENT[item.status].TEXT}
-					</Text>
-				</View>
-			</View>
-		);
-	}
-
 	return (
 		<>
 			<Agenda
 				items={items}
 				pastScrollRange={0}
 				futureScrollRange={CALENDAR.MAX_MONTH}
-				renderItem={item => renderItem(item)}
+				renderItem={item => <AppointmentItem item={item} />}
 			/>
 		</>
 	)
 };
-
-const styles = StyleSheet.create({
-	item: {
-		backgroundColor: '#FFF',
-		padding: 20,
-		margin: 10,
-	},
-	subItem: {
-		flexDirection: 'row',
-	},
-	hour: {
-		color: '#45535e',
-	},
-	patient: {
-		color: '#4b5863',
-		fontSize: 15,
-		marginTop: 10,
-	},
-	treatments: {
-		marginTop: 10,
-		color: '#9caebb',
-	},
-	image: {
-		width: 70,
-		height: 70,
-		borderRadius: 100,
-	},
-	statusContainer: {
-		marginTop: 15,
-		alignSelf: 'center',
-		flexDirection: 'row',
-	},
-	statusText: {
-		marginLeft: 5,
-	},
-});
 
 export default ClientCalendar;
