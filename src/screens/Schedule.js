a67cf3c733e93984ccf0dae379470b6b0270c854
Brendon Mota <bmota@its.jnj.com>
2021-01-30 18:29:21 -0300

    Starting Modal values

--- src/screens/Schedule.js
+++ src/screens/Schedule.js
@@ -7,13 +7,17 @@ import Appointment from '../services/Appointment';
 import User from '../services/User';
 import AppointmentItem from '../components/AppointmentItem';
 import PrimaryButton from '../components/PrimaryButton';
+import DefaultModal from '../components/DefaultModal';
+import Section from '../components/Section';
 import { StyleSheet, View } from 'react-native';
+import DefaultButton from '../components/DefaultButton';
 
 LocaleConfig.defaultLocale = 'pt-BR';
 
 function Schedule({ setRoute }) {
   const [user, setUser] = useState();
   const [items, setItems] = useState({});
+  const [appointment, setAppointment] = useState();
 
   useEffect(() => {
     load();
@@ -49,10 +53,6 @@ function Schedule({ setRoute }) {
     return data.reduce((obj, value) => Object.assign(obj, value), {});
   }
 
-  function showModal() {
-    console.log('Modal Here');
-  }
-
   return (
     <>
       <PrimaryButton style={styles.back}
@@ -64,10 +64,35 @@ function Schedule({ setRoute }) {
           pastScrollRange={0}
           futureScrollRange={CALENDAR.MAX_MONTH}
           renderItem={item =>
-            <AppointmentItem item={item} disabled={!user.admin} onPress={() => showModal()} />
+            <AppointmentItem
+              item={item} disabled={!user.admin}
+              onPress={() => setAppointment(item)} />
           }
         />
       </View>
+      <DefaultModal
+        isModalVisible={!!appointment}
+        setIsModalVisible={setAppointment}>
+        {
+          appointment &&
+          <>
+            <Section
+              title='Aprovamento de consulta'
+              description={appointment._id}
+            />
+            <View style={styles.buttonsContainer}>
+              <DefaultButton style={styles.rejectButton} relativeIcon={true}
+                icon='close' text='Rejeitar' isLeft={true}
+                onClick={() => setRoute('Actions')}
+              />
+              <DefaultButton style={styles.acceptButton} relativeIcon={true}
+                icon='check' text='Aceitar'
+                onClick={() => createAppointment()}
+              />
+            </View>
+          </>
+        }
+      </DefaultModal>
     </>
   )
 };
@@ -78,6 +103,19 @@ const styles = StyleSheet.create({
   },
   container: {
     height: '100%',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+  },
+  rejectButton: {
+    flex: 0.5,
+    marginRight: 10,
+    backgroundColor: '#c71441',
+  },
+  acceptButton: {
+    flex: 0.5,
+    marginLeft: 10,
   },
 });
 
