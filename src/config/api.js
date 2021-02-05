dac7f4ac3e084034db546abce4114737a61283c5
Brendon Mota <bmota@its.jnj.com>
2021-02-05 14:40:04 -0300

    Use .env

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,6 @@
-export const API = 'http://192.168.0.16:3000';
+import { API_URL } from "@env";
+
+export const API = API_URL;
 
 export const GOOGLE_AUTH = {
   webClientId: '1080313855309-q3hpbd4redqsqq22qgah3q42dopd19da.apps.googleusercontent.com',
