99fb0c38450243f4f4a85dccb2b8065bb26a6ef5
Brendon Mota <bmota@its.jnj.com>
2021-02-10 15:01:04 -0300

    Adding WEB Client ID

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,5 +1,13 @@
-import { API_URL, ANDROID_CLIENT_ID } from "@env";
+import {
+  API_URL,
+  ANDROID_CLIENT_ID,
+  WEB_CLIENT_ID
+} from "@env";
 
 export const API = API_URL;
 
-export const GOOGLE_AUTH = { androidClientId: ANDROID_CLIENT_ID };
+export const GOOGLE_AUTH = {
+  androidClientId: ANDROID_CLIENT_ID,
+  webClientId: WEB_CLIENT_ID,
+  offlineAccess: true,
+};
