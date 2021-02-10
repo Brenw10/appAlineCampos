d242229d3069aced636dbab65ca01c4f8e35caf3
Brendon Mota <bmota@its.jnj.com>
2021-02-10 14:16:52 -0300

    Updating

--- src/config/Api.js
+++ src/config/Api.js
@@ -3,6 +3,5 @@ import { API_URL } from "@env";
 export const API = API_URL;
 
 export const GOOGLE_AUTH = {
-  webClientId: '1080313855309-q3hpbd4redqsqq22qgah3q42dopd19da.apps.googleusercontent.com',
-  offlineAccess: true,
+  androidClientId: '1080313855309-9tc4f9bpatk1lgeopcu96bligrq91jte.apps.googleusercontent.com',
 };
