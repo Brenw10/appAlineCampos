0acfd6a9b3d85d53c84c2777e00f8ef2efc6939f
Brendon Mota <bmota@its.jnj.com>
2021-02-10 14:40:14 -0300

    Getting android client from env

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,7 +1,5 @@
-import { API_URL } from "@env";
+import { API_URL, ANDROID_CLIENT_ID } from "@env";
 
 export const API = API_URL;
 
-export const GOOGLE_AUTH = {
-  androidClientId: '1080313855309-9tc4f9bpatk1lgeopcu96bligrq91jte.apps.googleusercontent.com',
-};
+export const GOOGLE_AUTH = { androidClientId: ANDROID_CLIENT_ID };
