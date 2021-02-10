9a6c0158ff4d36a63c9f89710279c2e3e2382e7d
Brendon Mota <bw.bwctm@gmail.com>
2021-02-10 16:50:54 -0300

    Doing things working in iOS

--- src/config/Api.js
+++ src/config/Api.js
@@ -1,13 +1,15 @@
 import {
   API_URL,
   ANDROID_CLIENT_ID,
-  WEB_CLIENT_ID
+  WEB_CLIENT_ID,
+  IOS_CLIENT_ID,
 } from "@env";
 
 export const API = API_URL;
 
 export const GOOGLE_AUTH = {
   androidClientId: ANDROID_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID,
   webClientId: WEB_CLIENT_ID,
   offlineAccess: true,
 };
